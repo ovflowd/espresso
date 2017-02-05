e6a24c768a6dff6d81cbc103953fde662a9c30af
Jeremy Castellano <jeremy.castellano@me.com>
2017-02-06 00:02:42 +0100

    Started staff logging

---
+++ app/Http/Controllers/LogsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Log;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LogsController extends Controller
+{
+
+    /**
+     * Create a new log inside the database
+     * @param Request $request
+     * @param $data
+     */
+    public function create(Request $request, $data)
+    {
+        $log = new Log();
+
+        $log->user_id = $request->user()->id ?? 0;
+        $log->address = $request->ip();
+        $log->request = $data;
+
+        $log->save();
+    }
+}
