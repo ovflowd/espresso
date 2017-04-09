074e6afce98c3ef9918851d38fb73d3543da5c01
Jeremy Castellano <jeremy.castellano@me.com>
2017-04-09 20:10:39 +0200

    Roles-related methods

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     protected $table = "espreso_roles";
+    public $timestamps = false;
 
     /**
      * Return role name from it's ID
@@ -18,5 +19,29 @@ class Role extends Model
     {
         $name = Role::where('id', $rid)->first();
         return $name->name;
+    }
+
+    public static function formatAssignment($assigned_roles)
+    {
+        $roles = explode(';', $assigned_roles);
+
+        foreach($roles as $role)
+        {
+            echo '<span class="label label-default">' . self::getRoleName($role) . '</span> ';
+        }
+    }
+
+    public function exists($rname)
+    {
+        $role = Role::where('name', $rname)->first()->name;
+
+        if($rname == $role)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
