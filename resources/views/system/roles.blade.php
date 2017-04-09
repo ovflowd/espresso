0b7c3a6fbafbcbaa50b2151853d9ede6392e2b28
Jeremy Castellano <jeremy.castellano@me.com>
2017-04-09 20:05:46 +0200

    Moved roles and users creation to on-page modal

--- resources/views/system/roles.blade.php
+++ resources/views/system/roles.blade.php
@@ -2,41 +2,78 @@
 
 @section('content')
     <section id="content">
-        <div class="content__header">
-        <h2>Account Roles</h2>
-        </div>
-        <div class="card">
-            <div class="card__header">
-                <h2>Roles <small>Roles are group of users that are set to specify which users can access which permissions</small></h2>
-
-                <div class="actions">
-                    <a href="/system/roles/add"><i class="zmdi zmdi-plus"></i></a>
-                </div>
+            <div class="content__header">
+                <h2>Account Roles</h2>
             </div>
+            <div class="card">
+                <div class="card__header">
+                    <h2>Roles <small>Roles are group of users that are set to specify which users can access which permissions</small></h2>
+                        <div class="actions">
+                            <a data-toggle="modal" data-target="#addRole" href="#"><i class="zmdi zmdi-plus"></i></a>
+                        </div>
+                    </div>
 
-            <div class="card__body">
-                <div class="table-responsive">
-                    <table class="espreso-mae-data-table table-striped">
-                        <thead>
-                        <tr>
-                            <th data-column-id="id" data-type="numeric">ID</th>
-                            <th data-column-id="name">Name</th>
-                            <th data-column-id="permission_set">Permission set</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($roles as $role)
-                            <tr>
-                                <td>{{ $role->id }}</td>
-                                <td>{{ $role->name }}</td>
-                                <td></td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                    <div class="card__body">
+                        <div class="table-responsive">
+                            <table class="espreso-mae-data-table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th data-column-id="id" data-type="numeric">ID</th>
+                                        <th data-column-id="name">Name</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($roles as $role)
+                                        <tr>
+                                            <td>{{ $role->id }}</td>
+                                            <td>{{ $role->name }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-
     </section>
+
+    <div class="modal fade" id="addRole">
+        <div class="modal-dialog">
+            <div class="modal-content modal-dark">
+                <div class="modal-header">
+                    <h4 class="modal-title modal-title-dark">
+                        Create new role<br><br/>
+                        <small>Roles are a set of permissions that can be assigned to a housekeeping user</small>
+                    </h4>
+                </div>
+                <form method="POST" action="/system/roles/add">
+                <div class="modal-body">
+                        <div class="input-group input-group-lg">
+                            <span class="input-group-addon"><i class="zmdi zmdi-info-outline"></i></span>
+                            <div class="form-group">
+                                <input type="text" id="id" name="id" class="form-control input-lg" placeholder="Role ID">
+                                <i class="form-group__bar"></i>
+                            </div>
+                        </div>
+                        <br/><span class="help-block">A role ID, is a number that defines the role identity within the database. To simplify the process, this field will be removed in a next update. This field must be unique</span>
+                        <div class="input-group input-group-lg">
+                            <span class="input-group-addon"><i class="zmdi zmdi-edit"></i></span>
+                            <div class="form-group">
+                                <input type="text" id="name" name="name" class="form-control input-lg" placeholder="Name">
+                                <i class="form-group__bar"></i>
+                            </div>
+                        </div>
+                        {{ csrf_field() }}
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-link btn-link-dark" data-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-link btn-link-dark">Add role</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $('#addRole').modal();
+    </script>
 @stop
--- resources/views/system/users.blade.php
+++ resources/views/system/users.blade.php
@@ -10,7 +10,7 @@
                 <h2>All Accounts <small>System Accounts are accounts that are used to log on the Housekeeping to execute actions</small></h2>
 
                 <div class="actions">
-                    <a href="/system/users/add"><i class="zmdi zmdi-account-add"></i></a>
+                    <a href="#" data-toggle="modal" data-target="#addSysAccount"><i class="zmdi zmdi-account-add"></i></a>
                 </div>
             </div>
 
@@ -43,4 +43,88 @@
         </div>
 
     </section>
+
+    <div class="modal fade" id="addSysAccount">
+        <div class="modal-dialog">
+            <div class="modal-content modal-dark">
+                <div class="modal-header">
+                    <h4 class="modal-title modal-title-dark">
+                        Create new system account<br><br/>
+                        <small>System accounts are different from game accounts, system accounts allows the authentication of a user in the Housekeeping</small>
+                    </h4>
+                </div>
+                <div class="modal-body">
+                    <div class="alert alert-info">
+                        <strong><i class="zmdi zmdi-info-outline"></i> Use a valid email address</strong><br/>
+                        A valid email address is required in order to send the account owner it's password.
+                    </div>
+
+                    <form method="POST" action="/system/users/add">
+                        <h5>Account info</h5>
+                        <div class="input-group input-group-lg">
+                            <span class="input-group-addon"><i class="zmdi zmdi-account"></i></span>
+                            <div id="username-group" class="form-group">
+                                <input type="text" id="username" name="username" class="form-control input-lg" placeholder="Username">
+                                <span class="help-block"></span>
+                                <i class="form-group__bar"></i>
+                            </div>
+                        </div>
+                        <div class="input-group input-group-lg">
+                            <span class="input-group-addon"><i class="zmdi zmdi-email"></i></span>
+                            <div class="form-group">
+                                <input type="text" id="email" name="email" class="form-control input-lg" placeholder="Email address">
+                                <i class="form-group__bar"></i>
+                            </div>
+                        </div>
+                        <br />
+                        <h5>Account role</h5>
+                        <div class="input-group input-group-lg">
+                            <span class="input-group-addon"><i class="zmdi zmdi-key"></i></span>
+                            <div class="form-group">
+                                <select class="select2" name="rank" data-placeholder="Select a role group">
+                                    @foreach($roles as $role)
+                                        <option value="{{ $role->id }}">{{ $role->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <br>
+                        {{ csrf_field() }}
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-link btn-link-dark" data-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-link btn-link-dark">Add account</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $('#addSysAccount').modal();
+
+        $('#username').keyup(function(e) {
+            e.preventDefault();
+
+            var usergroup = $('#username-group');
+
+            $.ajax({
+                url: '/internal/sys_users/check/'+ $('#username').val(),
+                type: 'GET',
+                success: function(data)
+                {
+                    if(data.exists == true)
+                    {
+                        usergroup.addClass('has-danger has-feedback');
+                        usergroup.find('.help-block').html("This username is already taken, please try another one.");
+                    }
+                    else
+                    {
+                        usergroup.addClass('has-success has-feedback');
+                        usergroup.find('.help-block').html("Good to go, this username is free to use.");
+                    }
+                }
+            })
+        });
+    </script>
 @stop
