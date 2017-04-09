c565dfe09363ae79c73a01715545d5d05ccdea71
Jeremy Castellano <jeremy.castellano@me.com>
2017-04-09 20:11:29 +0200

    Roles and perms creation methods

--- app/Http/Controllers/SystemController.php
+++ app/Http/Controllers/SystemController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Role;
 use App\Models\User;
 use App\Http\Controllers\LogsController as Logs;
+use App\Http\Controllers\PermissionController as Permissions;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -12,10 +13,16 @@ use Illuminate\Support\Facades\Hash;
 class SystemController extends Controller
 {
     private $log;
+    private $perm;
+    private $role;
 
-    public function __construct(Logs $logManager)
+    private $page_group = 'system';
+
+    public function __construct(Logs $logManager, Permissions $permManager, Role $roleModel)
     {
         $this->log = $logManager;
+        $this->perm = $permManager;
+        $this->role = $roleModel;
     }
 
     /**
@@ -42,8 +49,9 @@ class SystemController extends Controller
         $this->validate($request, [
             'username' => 'required|min:3|max:15|unique:espreso_users',
             'email' => 'required|email|unique:espreso_users',
-            'password' => 'required|min:8'
         ]);
+
+
 
         $user = new User();
         $user->username = $request->input('username');
@@ -57,14 +65,43 @@ class SystemController extends Controller
         return redirect()->intended('/system/users');
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function add_role_view(Request $request)
     {
         $request->request->add(['required_perm' => 'system_roles_write']);
         return view('system.roles.add')->with(['page_title' => 'Add account role']);
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function add_role(Request $request)
     {
+        $request->request->add(['required_perm' => 'system_roles_write']);
 
+        $this->validate($request, [
+            'id' => 'required|integer|unique:espreso_roles',
+            'name' => 'required|unique:espreso_roles'
+        ]);
+
+        $role = new Role();
+        $role->id = $request->input('id');
+        $role->name = $request->input('name');
+
+        $role->save();
+
+        /* New roles should automatically have login and dashboard access permissions */
+        $this->perm->update('login', $request->input('id'));
+        $this->perm->update('dashboard_access', $request->input('id'));
+
+        $this->log->create($request, 'ACTION', 'Added a new role with name: ' . $request->input('name'));
+        $request->session()->flash('success', 'Role ' . $request->input('name') . ' added successfully');
+        return redirect()->intended('/system/roles');
     }
+
+
 }
